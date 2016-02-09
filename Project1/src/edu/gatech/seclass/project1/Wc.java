696fc2a8a5c782c83ae34d32a57a45e2e34f54cf
Daniel Dersch <dhdersch@gatech.edu>
2016-02-09 10:51:44 -0500

    Implemented core functionality

--- Project1/src/edu/gatech/seclass/project1/Wc.java
+++ Project1/src/edu/gatech/seclass/project1/Wc.java
@@ -1,14 +1,29 @@
 package edu.gatech.seclass.project1;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
 public class Wc {
 	
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException, 
+	FileNotFoundException, IOException {
 		
-		for (String arg : args) {
-			System.out.println(arg);
-		}
+		CommandLineArguments arguments = new CommandLineArguments();
+		arguments.setArgs(args);
+		
+		Reader r = Core.LoadFileStream(arguments.getFilePath());
+		
+		Core c = new Core(arguments.getDelimiters(), arguments.getMinWordLength());
+		
+		double result = c.Calculate(r);
+		
+		r.close();
+		
+		System.out.println(result);
 
 	}
 	
