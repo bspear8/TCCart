54406adc7fe271af5dafecd9a051ff258fb1b439
Blaine Spear <blaine.spear@gmail.com>
2016-02-11 20:00:26 -0600

    Resolved "no delimiters" and "empty file" test

--- Project1/src/edu/gatech/seclass/project1/Core.java
+++ Project1/src/edu/gatech/seclass/project1/Core.java
@@ -9,14 +9,20 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-
+/**
+ * Core Class
+ * Takes the arguments from the Wc main function, reads the characters in 
+ * and counts each word, then returns the average word length.
+ */
 public class Core {
 	
 	private int _minWordLength;
 	private HashSet<Character> _delimiters;
 	private InputStreamReader _r;
 	
-	
+	/*
+	 * Constructor
+	 */
 	public Core(char[] delimiters, int minWordLength) {
 		this._minWordLength = minWordLength;
 		this._delimiters = new HashSet<Character>();
@@ -26,19 +32,29 @@ public class Core {
 		}
 	}
 	
+	/*
+	 * Returns true if the character is in the set of delimiters
+	 */
 	private Boolean isDelimiter(char c) {
 		
 		return this._delimiters.contains(new Character(c));
 	}
 	
+	/*
+	 * Where the magic happens.
+	 * Counts words (delimited by white space)
+	 * Counts sentences (delimited by delimiter character set)
+	 */
 	public double Calculate() throws IOException {
 		
 		ArrayList<Double> sentenceLengths = new ArrayList<Double>();
 		
+		boolean empty = true;
 		int i;
 		int currentNumWords = 0;
 		int currentWordLength = 0;
 		for (i = this._r.read(); i != -1; i = this._r.read()) {
+			empty = false;
 			
 			char ch = (char) i;
 			
@@ -63,10 +79,19 @@ public class Core {
 			}
 		
 		}
+		if(empty){
+			return 0;
+		}
+		if(sentenceLengths.isEmpty()){
+			return currentNumWords;
+		}
 		
 		return this.average(sentenceLengths);
 	}
 	
+	/*
+	 * Calculates average
+	 */
 	private double average(ArrayList<Double> lengths) {
 		
 		double total = 0.0;
@@ -78,6 +103,9 @@ public class Core {
 		return total / ((double)lengths.size());
 	}
 	
+	/*
+	 * IO Function
+	 */
 	public void LoadFileStream(String filePath) throws UnsupportedEncodingException, FileNotFoundException {
 		FileInputStream in = new FileInputStream(filePath);
 		this._r = new InputStreamReader(in, "US-ASCII");
