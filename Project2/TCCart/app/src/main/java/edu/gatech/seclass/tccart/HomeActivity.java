352dab7283446638294f9ccba7344263cb7aeba8
Daniel Dersch <dhdersch@gmail.com>
2016-03-15 12:19:52 -0400

    Initial commit

---
+++ Project2/TCCart/app/src/main/java/edu/gatech/seclass/tccart/HomeActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.tccart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
